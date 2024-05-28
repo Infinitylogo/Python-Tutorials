83de45f28d060ccb609a9de871782d46edfb7d70
InfinityLogo <ritura609@gmail.com>
2024-05-28 21:44:48 +0530

    changes in git commands

--- Git_Commands.py
+++ Git_Commands.py
@@ -4,7 +4,13 @@ git init
 
 ## how we can configure and init
 
+git remote add origin https://github.com/Infinitylogo/Python-Tutorials.git
+
+git status 
+
 ## how we can add
+
+git add . or file name
 
 ## how we can commit 
 
