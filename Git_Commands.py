28e0eaca05e84b7ffef289922123c80d65141445
InfinityLogo <ritura609@gmail.com>
2024-05-28 21:48:19 +0530

    changes in git commands

--- Git_Commands.py
+++ Git_Commands.py
@@ -3,7 +3,6 @@
 git init
 
 ## how we can configure and init
-
 git remote add origin https://github.com/Infinitylogo/Python-Tutorials.git
 
 git status 
@@ -13,5 +12,16 @@ git status
 git add . or file name
 
 ## how we can commit 
+git commit -m 'comments'
 
 ## how we can push and pull the changes 
+git push origin main/dev
+
+## clone 
+
+git clone -b branch_name 'https://github.com/Infinitylogo/Python-Tutorials.git'
+
+
+# switch to specfic branch
+git checkout branch_name
+git switch branch_name
